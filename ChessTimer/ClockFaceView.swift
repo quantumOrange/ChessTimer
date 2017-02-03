27d9c2f11a41e11febf34138df749bc252c85ccc
David Crooks <crodave@gmail.com>
2017-02-03 21:06:18 +0100

    initial commit

---
+++ ChessTimer/ClockFaceView.swift
@@ -0,0 +1,39 @@
+//
+//  ClockFaceView.swift
+//  ChessTimer
+//
+//  Created by David Crooks on 26/01/2017.
+//  Copyright © 2017 David Crooks. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class ClockFaceView: UIView {
+    var clockFace:ClockFace = ClockFace(center:CGPoint(),radius:0.0,angle:0.0) {
+        didSet {
+            print("set clockFace \(clockFace)")
+            clockFace.faceColor = faceColor
+            setNeedsDisplay()
+        }
+    }
+    
+    @IBInspectable var faceColor:UIColor = UIColor.black { didSet {
+            clockFace.faceColor = faceColor
+            setNeedsDisplay()
+        }
+    }
+    
+    
+    var isActive = false
+
+    override func draw(_ rect: CGRect) {
+        let hue:CGFloat = 240.0/360.0
+        let brightness:CGFloat = 0.9
+    clockFace.timeColor = isActive ? UIColor(hue: hue, saturation: 0.5, brightness: brightness, alpha: 1.0) : UIColor(hue: hue, saturation: 0.25, brightness: brightness, alpha: 1.0)
+        
+        if let context = UIGraphicsGetCurrentContext() {
+            clockFace.draw(renderer: TestRenderer())
+            clockFace.draw(renderer: context)
+        }
+    }
+}
---
+++ ChessTimer/PlayerTimerView.swift
@@ -0,0 +1,28 @@
+//
+//  PlayerTimerView.swift
+//  ChessTimer
+//
+//  Created by David Crooks on 26/01/2017.
+//  Copyright © 2017 David Crooks. All rights reserved.
+//
+
+import UIKit
+
+class PlayerTimerView: UIStackView {
+    
+    var isActive =  false
+    
+    @IBInspectable var player:Player = .white
+    
+    
+    //var view:UIStackView
+    /*
+     // Only override draw() if you perform custom drawing.
+     // An empty implementation adversely affects performance during animation.
+     override func draw(_ rect: CGRect) {
+     // Drawing code
+     }
+     */
+    
+}
+
